fbcdbe0a7e4cc2e5d31a8f03ddb223a1e15b44bd
Mohamad Alsadhan <mo@mosadhan.com>
2025-12-16 06:15:29 +0300

    uncringify

--- footer.js
+++ footer.js
@@ -1,10 +1,2 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('footer').innerHTML = '   <p>Have something important to say? Here\'s my <a href="publickey.pgp">PGP key</a> (fbi have my emails dont be silly)</p>' +
-    '   <div id="icons" >' +
-    '   <a href="mailto:mo@mosadhan.com"><i class="fa-solid fa-at fa-3x"></i></a>' +
-    '   <a href="https://github.com/mqqz"> <i class="fa-brands fa-github fa-3x"></i></a>' +
-    '   <a href="https://www.linkedin.com/in/mohamad-alsadhan/"> <i class="fa-brands fa-linkedin fa-3x"></i></a>' +
-    '</div>';
-});
 
 
